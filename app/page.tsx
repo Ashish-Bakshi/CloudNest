58dc85facffa75e759bbbb82dcc0a995bd70153c
Ashish Bakshi <ashishbakshi2004@gmail.com>
2025-07-31 19:01:01 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl font-bold text-brand">CloudNest - Your Cloud Storage Solution</h1>
+    </div>
+  );
+}
